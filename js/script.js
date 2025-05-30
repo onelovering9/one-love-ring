c76212c0b0d58ada9dd428dc6883cf1bd51aa7e6
onelovering9 <facilbuyshop@gmail.com>
2025-05-30 20:26:52 +0200

    Fix Snipcart URL and mobile menu styles

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const hamburger = document.getElementById('hamburger');
-  const navMenu = document.getElementById('nav-menu');
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("nav-menu");
 
-  hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('show');
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
   });
 });
